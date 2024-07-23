004ab09a776d956bce473b925d7eee9753ddf09e
andrewras58 <andrewras58@gmail.com>
2024-07-23 11:24:06 -0700

    html skeleton

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,27 @@
-import './App.scss';
+import './css/App.scss';
+
+import Navbar from './Navbar';
+import Home from './Home';
+import Footer from './Footer';
 
 function App() {
   return (
-    <span>hello world</span>
-    // nav bar component
-    // hero section component
-    // about section component
-    // feature section component
-    // experience section component
-    // contact section component
-    // footer section component
+    <div className="App">
+      <Navbar />
+      <Home />
+      <Footer />
+      {
+      // Use this code if you ever use multiple pages (HashRouter if github pages, Browser Router for AWS)
+      // <HashRouter>
+      //   <Navbar />
+      //   <Routes>
+      //     <Route path="/" element={<Home />}/>
+      //     <Route path="/newpage" element={<Newpage />}/>
+      //   </Routes>
+      //   <Footer />
+      // </HashRouter>
+      }
+    </div>
   );
 }
 
